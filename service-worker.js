59c66b60ad51991a3df88b3163ee45b2646bb273
t.ikeda.mk <t.ikeda.mk@gmail.com>
2019-09-23 18:32:40 +0900

    Add save/load cache

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,31 @@
+var CACHE_NAME = 'pwa-sample-caches';
+
+var urlsToCache = [
+    '/maruko1230.github.io/',
+];
+
 self.addEventListener('install', function(e) {
     console.log('[ServiceWorker] Install');
+
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    )
 });
 
 self.addEventListener('activate', function(e) {
     console.log('[ServiceWorker] Activate');
 });
 
-self.addEventListener('fetch', function(e) {});
+self.addEventListener('fetch', function(e) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response ? response : fetch(event.request);
+            })
+    )
+});
